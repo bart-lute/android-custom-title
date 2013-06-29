39e6a0da7f8c5573b829c65453971a0cc6e22cdd
Bart Lute <bart.lute@gmail.com>
2013-06-29 12:47:27 +0200

    Indeterminate spinner in Title Bar

--- src/nl/bartlute/androidcustomtitle/CustomTitle.java
+++ src/nl/bartlute/androidcustomtitle/CustomTitle.java
@@ -3,13 +3,16 @@ package nl.bartlute.androidcustomtitle;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class CustomTitle extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		getWindow().requestFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_title);
+		setProgressBarIndeterminateVisibility(true);
 	}
 
 	@Override
